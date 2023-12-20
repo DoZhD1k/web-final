10f6be7aa3ee663c0586fc37d1a076a62688e079
DoZhD1k <chereshnikovarlen03@gmail.com>
2023-12-20 18:18:46 +0600

    added vite

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ShopContextProvider from './Context/ShopContext';
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+ 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ShopContextProvider>
-        <App />
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+            <App />
+        </ClerkProvider>
     </ShopContextProvider>
 );
