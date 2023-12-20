20b44b99b012bb0a587c0f7827c565fe53f57ff8
DoZhD1k <chereshnikovarlen03@gmail.com>
2023-12-20 18:02:12 +0600

    before api

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,10 +17,10 @@ const Navbar = () => {
         <p>E-commerce</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none' }} to="womens">Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"}</li>
+        <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu==="mens"}</li>
+        <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none' }} to="womens">Women</Link>{menu==="womens"}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu==="kids"}</li>
       </ul>
       <div className="nav-login-cart">
         <Link to='/login'><button>Sign Up</button></Link>
